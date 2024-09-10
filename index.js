a769149c97e1468b26e643cb4f25e787a7b21e0f
sriramManthina <msriramvarma04@gmail.com>
2024-09-10 10:35:05 +0530

    something

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const fs = require("fs");
+const flattenArrayOfObjects = require('./utils/jsonManipulationMethods').flattenArrayOfObjects
 
-const port = 3000;
+const port = 3001;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
@@ -59,6 +60,59 @@ app.get("/users/:account_ids", (req, res) => {
     }
   });
 });
+
+
+// Endpoint that takes comma-separated account_id as URL params
+app.get("/users-specific", (req, res) => {
+  const accountIds = req.query.ids ? req.query.ids.split(","): null;
+  const cols = req.query.cols ? req.query.cols.split(",") : null;
+  const filePath = path.join(__dirname, "data.json");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to read the JSON file." });
+    }
+
+    try {
+      const usersData = JSON.parse(data);
+
+      let filteredData = usersData
+
+      // If ids parameter is given, filter the keys
+      if (accountIds){
+        filteredData = usersData.filter((user) =>
+          accountIds.includes(user.account_id.toString())
+        );
+      }
+      
+      console.log(filteredData)
+      console.log('__________________---')
+      filteredData = flattenArrayOfObjects(filteredData, '__')
+      console.log(filteredData)
+      // If cols parameter is given, filter the keys
+      if (cols) {
+        const result = filteredData.map((user) => {
+          const filteredUser = {};
+          cols.forEach((col) => {
+            if (user.hasOwnProperty(col)) {
+              filteredUser[col] = user[col];
+            } else {
+              filteredUser[col] = 'n-a'
+            }
+          });
+          return filteredUser;
+        });
+        res.status(200).json(result);
+      } else {
+        res.status(200).json(filteredData);
+      }
+    } catch (parseError) {
+      res.status(500).json({ error: "Failed to parse the JSON file." });
+    }
+  });
+});
+
+
 
 app.get("/unique-account-ids", (req, res) => {
   fs.readFile(path.join(__dirname, "data.json"), "utf8", (err, data) => {
